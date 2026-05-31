c873cd89f8410c3c126060b5218490e0e9241402
Varshith <kotipallianjan@gmail.com>
2026-05-31 17:20:16 +0530

    lite task server working

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
+
+type Task struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Status      bool   `json:"status"`
+	Priority    int    `json:"priority"`
+}
+
+var tasks []Task
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
@@ -13,27 +23,43 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "application/json")
 
-	fmt.Fprintf(w, "Hello Echo")
+	for _, task := range tasks {
+		taskJson, err := json.Marshal(task)
+		if err != nil {
+			http.Error(w, "Failed to marshal task", http.StatusInternalServerError)
+			return
+		}
+		// w.Write(taskJson)
+		// w.Write([]byte("\n"))
+		fmt.Fprintf(w, "%s\n", string(taskJson))
+	}
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed) //405 Method Not Allowed
 		return
 	}
 
 	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
 
 	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError) //500 Internal Server Error
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain")
-
-	fmt.Fprintf(w, "Received Post req with body: %s", string(body))
+	var newTask Task
+	err = json.Unmarshal(body, &newTask)
+	if err != nil {
+		http.Error(w, "Failed to parse JSON", http.StatusBadRequest) //400 Bad Request
+		return
+	}
+	tasks = append(tasks, newTask)
+	w.WriteHeader(http.StatusCreated) //201 Created
+	w.Write([]byte("Task Created Successfully\n"))
 
 }
 func main() {
